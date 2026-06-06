aba8f5b6052e0ee8037a9d6a703bd5a569d05517
brewek <15088509+brewek@users.noreply.github.com>
2026-06-06 19:33:23 +0200

    feat: config provenance for npm publish

--- src/custom-elements.d.ts
+++ src/custom-elements.d.ts
@@ -1,12 +1,13 @@
 import React from 'react';
 
-declare global {
+declare module 'react' {
   namespace JSX {
     interface IntrinsicElements {
       'chess-board': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
         fen?: string;
         orientation?: 'white' | 'black';
         coordinates?: 'none' | 'classic' | 'full';
+        class?: string;
       };
     }
   }
